4e64d9ac9249edcf2a56044e5fa4b2aac4551ce8
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-09-21 10:25:56 -0700

    Move all node manipulation into Node.
    Simplifies a bit of phrasing and makes it easier to see where bugs might be
    lurking.

--- blackjack.py
+++ blackjack.py
@@ -1,246 +1,243 @@
 from collections import namedtuple
 from operator import itemgetter
 
-Node = namedtuple("Node", "value, left, right, red")
+class Node(namedtuple("Node", "value, left, right, red")):
+    __slots__ = ()
+
+    def find(self, value, key):
+        """
+        Find a value in a node, using a key function.
+        """
+
+        while self is not NULL:
+            direction = cmp(key(value), key(self.value))
+            if direction < 0:
+                self = self.left
+            elif direction > 0:
+                self = self.right
+            elif direction == 0:
+                return self.value
+
+    def rotate_left(self):
+        """
+        Rotate the node to the left.
+        """
+
+        right = self.right
+        new = self._replace(right=self.right.left, red=True)
+        top = right._replace(left=new, red=self.red)
+        return top
+
+    def rotate_right(self):
+        """
+        Rotate the node to the right.
+        """
+
+        left = self.left
+        new = self._replace(left=self.left.right, red=True)
+        top = left._replace(right=new, red=self.red)
+        return top
+
+    def flip(self):
+        """
+        Flip colors of a node and its children.
+        """
+
+        left = self.left._replace(red=not self.left.red)
+        right = self.right._replace(red=not self.right.red)
+        top = self._replace(left=left, right=right, red=not self.red)
+        return top
+
+    def balance(self):
+        """
+        Balance a node.
+
+        The balance is inductive and relies on all subtrees being balanced
+        recursively or by construction. If the subtrees are not balanced, then
+        this will not fix them.
+        """
+
+        # Always lean left with red nodes.
+        if self.right.red:
+            self = self.rotate_left()
+
+        # Never permit red nodes to have red children. Note that if the left-hand
+        # node is NULL, it will short-circuit and fail this test, so we don't have
+        # to worry about a dereference here.
+        if self.left.red and self.left.left.red:
+            self = self.rotate_right()
+
+        # Finally, move red children on both sides up to the next level, reducing
+        # the total redness.
+        if self.left.red and self.right.red:
+            self = self.flip()
+
+        return self
+
+    def insert(self, value, key):
+        """
+        Insert a value into a tree rooted at the given node.
+
+        Balances the tree during insertion.
+
+        An update is performed instead of an insertion if a value in the tree
+        compares equal to the new value.
+        """
+
+        # Base case: Insertion into the empty tree is just creating a new node
+        # with no children.
+        if self is NULL:
+            return Node(value, NULL, NULL, True)
+
+        # Recursive case: Insertion into a non-empty tree is insertion into
+        # whichever of the two sides is correctly compared.
+        direction = cmp(key(value), key(self.value))
+        if direction < 0:
+            left = self.left.insert(value, key)
+            self = self._replace(left=left)
+        elif direction > 0:
+            right = self.right.insert(value, key)
+            self = self._replace(right=right)
+        elif direction == 0:
+            # Exact hit on an existing node (this node, in fact). In this
+            # case, perform an update.
+            self = self._replace(value=value)
+
+        # And balance on the way back up.
+        return self.balance()
+
+    def move_red_left(self):
+        """
+        Shuffle red to the left of a tree.
+        """
+
+        self = self.flip()
+        if self.right is not NULL and self.right.left.red:
+            self = self._replace(right=self.right.rotate_right())
+            self = self.rotate_left().flip()
+
+        return self
+
+    def move_red_right(self):
+        """
+        Shuffle red to the right of a tree.
+        """
+
+        self = self.flip()
+        if self.left is not NULL and self.left.left.red:
+            self = self.rotate_right().flip()
+
+        return self
+
+    def delete_min(self):
+        """
+        Delete the left-most value from a tree.
+        """
+
+        # Base case: If there are no nodes lesser than this node, then this is the
+        # node to delete.
+        if self.left is NULL:
+            return NULL, self.value
+
+        # Acquire more reds if necessary to continue the traversal. The
+        # double-deep check is fine because NULL is red.
+        if not self.left.red and not self.left.left.red:
+            self = self.move_red_left()
+
+        # Recursive case: Delete the minimum node of all nodes lesser than this
+        # node.
+        left, value = self.left.delete_min()
+        self = self._replace(left=left)
+
+        return self.balance(), value
+
+    def delete_max(self):
+        """
+        Delete the right-most value from a tree.
+        """
+
+        # Attempt to rotate left-leaning reds to the right.
+        if self.left.red:
+            self = self.rotate_right()
+
+        # Base case: If there are no selfs greater than this self, then this is
+        # the self to delete.
+        if self.right is NULL:
+            return NULL, self.value
+
+        # Acquire more reds if necessary to continue the traversal. NULL is
+        # red so this check doesn't need to check for NULL.
+        if not self.right.red and not self.right.left.red:
+            self = self.move_red_right()
+
+        # Recursive case: Delete the maximum self of all selfs greater than this
+        # self.
+        right, value = self.right.delete_max()
+        self = self._replace(right=right)
+
+        return self.balance(), value
+
+    def delete(self, value, key):
+        """
+        Delete a value from a tree.
+        """
+
+        # Base case: The empty tree cannot possibly have the desired value.
+        if self is NULL:
+            raise KeyError(value)
+
+        direction = cmp(key(value), key(self.value))
+
+        # Because we lean to the left, the left case stands alone.
+        if direction < 0:
+            if not self.left.red and not self.left.left.red:
+                self = self.move_red_left()
+            # Delete towards the left.
+            left = self.left.delete(value, key)
+            self = self._replace(left=left)
+        else:
+            # If we currently lean to the left, lean to the right for now.
+            if self.left.red:
+                self = self.rotate_right()
+
+            # Best case: The node on our right (which we just rotated there) is a
+            # red link and also we were just holding the node to delete. In that
+            # case, we just rotated NULL into our current node, and the node to
+            # the right is the lone matching node to delete.
+            if direction == 0 and self.right is NULL:
+                return NULL
+
+            # No? Okay. Move more reds to the right so that we can continue to
+            # traverse in that direction. At *this* spot, we do have to confirm
+            # that node.right is not NULL...
+            if (not self.right.red and
+                self.right is not NULL and
+                not self.right.left.red):
+                self = self.move_red_right()
+
+            if direction > 0:
+                # Delete towards the right.
+                right = self.right.delete(value, key)
+                self = self._replace(right=right)
+            else:
+                # Annoying case: The current node was the node to delete all
+                # along! Use a right-handed minimum deletion. First find the
+                # replacement value to rebuild the current node with, then delete
+                # the replacement value from the right-side tree. Finally, create
+                # the new node with the old value replaced and the replaced value
+                # deleted.
+                rnode = self.right
+                while rnode is not NULL:
+                    replacement = rnode.value
+                    rnode = rnode.left
+
+                right = self.right.delete_min()
+                self = self._replace(value=replacement, right=right)
+
+        return self.balance()
+
 
 NULL = Node(None, None, None, False)
-
-
-def find(node, value, key):
-    """
-    Find a value in a node, using a key function.
-    """
-
-    while node is not NULL:
-        direction = cmp(key(value), key(node.value))
-        if direction < 0:
-            node = node.left
-        elif direction > 0:
-            node = node.right
-        elif direction == 0:
-            return node.value
-
-
-def rotate_left(node):
-    """
-    Rotate the node to the left.
-    """
-
-    right = node.right
-    new = node._replace(right=node.right.left, red=True)
-    top = right._replace(left=new, red=node.red)
-    return top
-
-
-def rotate_right(node):
-    """
-    Rotate the node to the right.
-    """
-
-    left = node.left
-    new = node._replace(left=node.left.right, red=True)
-    top = left._replace(right=new, red=node.red)
-    return top
-
-
-def flip(node):
-    """
-    Flip colors of a node and its children.
-    """
-
-    left = node.left._replace(red=not node.left.red)
-    right = node.right._replace(red=not node.right.red)
-    top = node._replace(left=left, right=right, red=not node.red)
-    return top
-
-
-def balance(node):
-    """
-    Balance a node.
-
-    The balance is inductive and relies on all subtrees being balanced
-    recursively or by construction.
-    """
-
-    # Always lean left with red nodes.
-    if node.right.red:
-        node = rotate_left(node)
-
-    # Never permit red nodes to have red children. Note that if the left-hand
-    # node is NULL, it will short-circuit and fail this test, so we don't have
-    # to worry about a dereference here.
-    if node.left.red and node.left.left.red:
-        node = rotate_right(node)
-
-    # Finally, move red children on both sides up to the next level, reducing
-    # the total redness.
-    if node.left.red and node.right.red:
-        node = flip(node)
-
-    return node
-
-
-def insert(node, value, key):
-    """
-    Insert a value into a tree rooted at the given node.
-
-    Balances the tree during insertion.
-
-    An update is performed instead of an insertion if a value in the tree
-    compares equal to the new value.
-    """
-
-    # Base case: Insertion into the empty tree is just creating a new node
-    # with no children.
-    if node is NULL:
-        return Node(value, NULL, NULL, True)
-
-    # Recursive case: Insertion into a non-empty tree is insertion into
-    # whichever of the two sides is correctly compared.
-    direction = cmp(key(value), key(node.value))
-    if direction < 0:
-        left = insert(node.left, value, key)
-        node = node._replace(left=left)
-    elif direction > 0:
-        right = insert(node.right, value, key)
-        node = node._replace(right=right)
-    elif direction == 0:
-        # Exact hit on an existing node. In this case, perform an update.
-        node = node._replace(value=value)
-
-    # And balance on the way back up.
-    return balance(node)
-
-
-def move_red_left(node):
-    """
-    Shuffle red to the left of a tree.
-    """
-
-    node = flip(node)
-    if node.right.left.red:
-        node = node._replace(right=rotate_right(node.right))
-        node = flip(rotate_left(node))
-
-    return node
-
-
-def move_red_right(node):
-    """
-    Shuffle red to the right of a tree.
-    """
-
-    node = flip(node)
-    if node.left.left.red:
-        node = flip(rotate_right(node))
-
-    return node
-
-
-def delete_min(node):
-    """
-    Delete the left-most value from a tree.
-    """
-
-    # Base case: If there are no nodes lesser than this node, then this is the
-    # node to delete.
-    if node.left is NULL:
-        return NULL, node.value
-
-    # Acquire more reds if necessary to continue the traversal.
-    if not node.left.red and not node.left.left.red:
-        node = move_red_left(node)
-
-    # Recursive case: Delete the minimum node of all nodes lesser than this
-    # node.
-    left, value = delete_min(node.left)
-    node = node._replace(left=left)
-
-    return balance(node), value
-
-
-def delete_max(node):
-    """
-    Delete the right-most value from a tree.
-    """
-
-    # Attempt to rotate left-leaning reds to the right.
-    if node.left.red:
-        node = rotate_right(node)
-
-    # Base case: If there are no nodes greater than this node, then this is
-    # the node to delete.
-    if node.right is NULL:
-        return NULL, node.value
-
-    # Acquire more reds if necessary to continue the traversal.
-    if not node.right.red and not node.right.left.red:
-        node = move_red_right(node)
-
-    # Recursive case: Delete the maximum node of all nodes greater than this
-    # node.
-    right, value = delete_max(node.right)
-    node = node._replace(right=right)
-
-    return balance(node), value
-
-
-def delete(node, value, key):
-    """
-    Delete a value from a tree.
-    """
-
-    # Base case: The empty tree cannot possibly have the desired value.
-    if node is NULL:
-        raise KeyError(value)
-
-    direction = cmp(key(value), key(node.value))
-
-    # Because we lean to the left, the left case stands alone.
-    if direction < 0:
-        if not node.left.red and not node.left.left.red:
-            node = move_red_left(node)
-        # Delete towards the left.
-        left = delete(node.left, value, key)
-        node = node._replace(left=left)
-    else:
-        # If we currently lean to the left, lean to the right for now.
-        if node.left.red:
-            node = rotate_right(node)
-
-        # Best case: The node on our right (which we just rotated there) is a
-        # red link and also we were just holding the node to delete. In that
-        # case, we just rotated NULL into our current node, and the node to
-        # the right is the lone matching node to delete.
-        if direction == 0 and node.right is NULL:
-            return NULL
-
-        # No? Okay. Move more reds to the right so that we can continue to
-        # traverse in that direction. At *this* spot, we do have to confirm
-        # that node.right is not NULL...
-        if not node.right.red and node.right.left and not node.right.left.red:
-            node = move_red_right(node)
-
-        if direction > 0:
-            # Delete towards the right.
-            right = delete(node.right, value, key)
-            node = node._replace(right=right)
-        else:
-            # Annoying case: The current node was the node to delete all
-            # along! Use a right-handed minimum deletion. First find the
-            # replacement value to rebuild the current node with, then delete
-            # the replacement value from the right-side tree. Finally, create
-            # the new node with the old value replaced and the replaced value
-            # deleted.
-            rnode = node.right
-            while rnode is not NULL:
-                replacement = rnode.value
-                rnode = rnode.left
-
-            right = delete_min(node.right)
-            node = node._replace(value=replacement, right=right)
-
-    return balance(node)
 
 
 class BJ(object):
@@ -263,7 +260,7 @@ class BJ(object):
                 self.add(item)
 
     def __contains__(self, value):
-        return find(self.root, value, self._key) is not None
+        return self.root.find(value, self._key) is not None
 
     def __len__(self):
         stack = [self.root]
@@ -292,20 +289,20 @@ class BJ(object):
                 node = node.right
 
     def add(self, value):
-        self.root = insert(self.root, value, self._key)
+        self.root = self.root.insert(value, self._key)
 
     def discard(self, value):
-        self.root = delete(self.root, value, self._key)
+        self.root = self.root.delete(value, self._key)
 
     def find(self, value):
-        return find(self.root, value, self._key)
+        return self.root.find(value, self._key)
 
     def pop_max(self):
-        self.root, value = delete_max(self.root)
+        self.root, value = self.root.delete_max()
         return value
 
     def pop_min(self):
-        self.root, value = delete_min(self.root)
+        self.root, value = self.root.delete_min()
         return value
 
 
@@ -352,21 +349,21 @@ class TestTrees(TestCase):
     def test_balance_right(self):
         node = Node(1, NULL, Node(2, NULL, NULL, True), False)
         balanced = Node(2, Node(1, NULL, NULL, True), NULL, False)
-        self.assertEqual(balance(node), balanced)
+        self.assertEqual(node.balance(), balanced)
 
     def test_balance_four(self):
         node = Node(2, Node(1, NULL, NULL, True), Node(3, NULL, NULL, True),
                     False)
         balanced = Node(2, Node(1, NULL, NULL, False),
                         Node(3, NULL, NULL, False), True)
-        self.assertEqual(balance(node), balanced)
+        self.assertEqual(node.balance(), balanced)
 
     def test_balance_left_four(self):
         node = Node(3, Node(2, Node(1, NULL, NULL, True), NULL, True), NULL,
                     False)
         balanced = Node(2, Node(1, NULL, NULL, False),
                         Node(3, NULL, NULL, False), True)
-        self.assertEqual(balance(node), balanced)
+        self.assertEqual(node.balance(), balanced)
 
 
 class TestBlackjack(TestCase):
